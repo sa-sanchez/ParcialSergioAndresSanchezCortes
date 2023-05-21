3185e089cc90053bbcad5f3ba3c61cafc5acf872
SergioSanchez <sasanchez.c96@gmail.com>
2023-05-21 15:55:34 -0500

    create Ticket entity and DataBaseContext

---
+++ TicketingSystem/TicketingSystem/DAL/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.DAL.Entities;
+
+namespace TicketingSystem.DAL
+{
+    public class DataBaseContext: DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+        }
+    }
+}
---
+++ TicketingSystem/TicketingSystem/DAL/Entities/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketingSystem.DAL.Entities
+{
+    public class Tickets
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? UseDate { get; set; }
+        public bool? isUsed { get; set; }
+        public string? EntranceGate { get; set; }
+    }
+}
